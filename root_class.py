cc5a8f753d46fc397aa4e073859edc4076c55a7d
hein <heinnyihtut@gmail.com>
2023-07-20 12:20:02 -0400

    Sync root_class.py

--- root_class.py
+++ root_class.py
@@ -11,7 +11,7 @@ class Root:
 
     EPSILON = 10E-10
 
-    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=(1,2) , verbose=False, minimize_objFunc=True):
+    def __init__(self, obj_func=None, lb=None, ub=None, problem_size=(1,2) , verbose=False, minimize_objFunc=False):
         """
         Parameters
         ----------
@@ -31,7 +31,7 @@ class Root:
         try:
             self.lb = lb * ones(problem_size)
             self.ub = ub * ones(problem_size)
-        except (TypeError, ValueError) as e:                                                                                # lb, ub must be integers and problem_size must be a tuple of integers
+        except (TypeError, ValueError) as e:                                                                         # lb, ub must be integers and problem_size must be a tuple of integers
             print(str(e))
             exit(0)
 
@@ -42,13 +42,13 @@ class Root:
 
     def get_position_fitness(self, position=None): return self.obj_func(position) if self.minimize_objFunc else 1.0 / (self.obj_func(position) + self.EPSILON)
 
-    def get_sorted_pop_and_global_best_agent(self, pop=None, id_best=None):                                 # biggest fitness --> smallest fitness
+    def sort_pop_and_get_global_best_agent(self, pop=None, id_best=None):                                          # biggest fitness --> smallest fitness
         """
-        Sort population and return the sorted population and the best position 
+        Sort population and return the best position 
         Method is only called once (during initialization of the slime mould agents)
         """
         pop.sort(key=lambda slime: slime.fitness, reverse=True)
-        return pop, deepcopy(pop[id_best])
+        return deepcopy(pop[id_best])
 
     def amend_position(self, position=None):
         return clip(position, self.lb, self.ub)
@@ -56,11 +56,11 @@ class Root:
     def amend_position_random(self, position=None):
         return where(logical_and(self.lb <= position, position <= self.ub), position, uniform(self.lb, self.ub))
 
-    def update_sorted_population_and_global_best_agent(self, pop=None, id_best=None, g_best=None):
+    def update_sorted_population_and_get_global_best_agent(self, pop=None, id_best=None, g_best=None):
         """ 
-        Sort the population, update the global best slime agent, and retun them
+        Sort the population, update the global best slime agent if necessary, and retun the latter
         """
         pop.sort(key=lambda slime: slime.fitness, reverse=True)
         current_best = pop[id_best]
-        g_best = deepcopy(current_best) if current_best.get_fitness() < g_best.get_fitness() else deepcopy(g_best)            # Compare global best slime mould w/ this batch's best slime mould
-        return sorted_pop, g_best
+        g_best = deepcopy(current_best) if current_best.get_fitness() > g_best.get_fitness() else g_best             # Compare global best slime mould w/ this batch's best slime mould
+        return g_best
